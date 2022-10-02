42f0e19b14341894d105721105c94c039fb0eece
Daniele Tosches <dta@fastwebnet.it>
2022-10-02 20:02:44 +0200

    Added MainViewModel and GameData classes, started creation of UI.

---
+++ GameManager/Models/GameData.cs
@@ -0,0 +1,51 @@
+﻿using GameLib.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameManager.Models
+{
+    /// <summary>
+    /// Piattaforma di gioco.
+    /// </summary>
+    public enum GamePlatform 
+    {
+        None,
+        Steam,
+        Origin,
+        EpicGames,
+        Ubisoft,
+        BattleNet,
+        Rockstar,
+        GOG
+    }
+
+    /// <summary>
+    /// Informazioni su un gioco.
+    /// </summary>
+    public class GameData
+    {
+        /// <summary>
+        /// Informazioni sul gioco.
+        /// </summary>
+        public IGame Data { get; }
+
+        /// <summary>
+        /// Piattaforma del gioco.
+        /// </summary>
+        public GamePlatform Platform { get; }
+
+        /// <summary>
+        /// Inizializza una nuova istanza di <see cref="GameData"/>.
+        /// </summary>
+        /// <param name="Data">Dati sul gioco.</param>
+        /// <param name="Platform">Piattaforma.</param>
+        public GameData(IGame Data, GamePlatform Platform)
+        {
+            this.Data = Data;
+            this.Platform = Platform;
+        }
+    }
+}
---
+++ GameManager/Viewmodels/MainViewModel.cs
@@ -0,0 +1,74 @@
+﻿using GameLib;
+using GameLib.Core;
+using GameManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameManager.Viewmodels
+{
+    /// <summary>
+    /// Viewmodel principale.
+    /// </summary>
+    public class MainViewModel
+    {
+        /// <summary>
+        /// Gestore delle informazioni sui lanucher.
+        /// </summary>
+        private readonly LauncherManager LauncherManager;
+
+        /// <summary>
+        /// Launcher disponibili.
+        /// </summary>
+        private readonly List<ILauncher> Launchers;
+
+        /// <summary>
+        /// Giochi installati.
+        /// </summary>
+        private readonly ObservableCollection<GameData> Games;
+
+        /// <summary>
+        /// Lista di giochi filtrata.
+        /// </summary>
+        public ObservableCollection<GameData> FilteredGames { get; init; }
+
+
+        public MainViewModel()
+        {
+            Games = new();
+            LauncherOptions Options = new()
+            {
+                LoadLocalCatalogData = true,
+                SearchGameConfigStore = false,
+                QueryOnlineData = true,
+                OnlineQueryTimeout = TimeSpan.FromSeconds(5),
+                SearchExecutables = true
+            };
+            LauncherManager = new(Options);
+            Launchers = LauncherManager.GetLaunchers().ToList();
+            GamePlatform Platform;
+            foreach (ILauncher launcher in Launchers)
+            {
+                Platform = launcher.Name switch
+                {
+                    "Steam" => GamePlatform.Steam,
+                    "Origin" => GamePlatform.Origin,
+                    "Epic Games" => GamePlatform.EpicGames,
+                    "GOG Galaxy" => GamePlatform.GOG,
+                    "Rockstar Games" => GamePlatform.Rockstar,
+                    "Ubisoft Connect" => GamePlatform.Ubisoft,
+                    "Battle.net" => GamePlatform.BattleNet,
+                    _ => GamePlatform.None,
+                };
+                foreach (IGame game in launcher.Games)
+                {
+                    Games.Add(new(game, Platform));
+                }
+            }
+            FilteredGames = new(Games);
+        }
+    }
+}
